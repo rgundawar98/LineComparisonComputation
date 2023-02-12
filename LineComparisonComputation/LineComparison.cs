d8f6caea6c0b111295bf6897cbd18f027d0a44f0
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-12 17:28:42 +0530

    Compared two lengths of line using Equal operator UC2_CompareTwoLines

--- LineComparisonComputation/LineComparison.cs
+++ LineComparisonComputation/LineComparison.cs
@@ -10,8 +10,8 @@ namespace LineComparisonComputation
     {
         public static void Comparison()
         {
-            int lengthx1 = 0, lengthy1 = 0, lengthx2 = 0, lengthy2 = 0;
-            double lineX=0,lineY=0 , Linelength1 =0;
+            int lengthx1 = 0, lengthy1 = 0, lengthx2 = 0, lengthy2 = 0 , lengthA1=0, lengthA2 = 0, lengthB1 = 0, lengthB2 = 0;
+            double lineX=0,lineY=0, Linelength1 =0, lineA=0, lineB =0,Linelength2 = 0;
 
             Console.WriteLine("Enter the length of lengthx1 and lengthx2");
             lengthx1 = Convert.ToInt32(Console.ReadLine());
@@ -27,6 +27,35 @@ namespace LineComparisonComputation
             //Calculating length of the line
             Linelength1 = Math.Sqrt(lineX + lineY);
             Console.WriteLine("The length of Line using cartesian points is :" + Linelength1);
+
+            Console.WriteLine("Enter the length of lengthA1 and lengthA2");
+            lengthA1 = Convert.ToInt32(Console.ReadLine());
+            lengthA2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the length of lengthB1 and lengthB2");
+            lengthB1 = Convert.ToInt32(Console.ReadLine());
+            lengthB2 = Convert.ToInt32(Console.ReadLine());
+            //Calculating Cartesian points
+            lineA = Math.Pow(lengthA2 - lengthA1, 2);
+            lineB = Math.Pow(lengthB2 - lengthB1, 2);
+            Console.WriteLine("The length of Cartesian point lineY :" +lineA);
+            Console.WriteLine("The length of Cartesian point lineB :" +lineB);
+            //Calculating length of the line
+            Linelength2 = Math.Sqrt(lineA + lineB);
+            Console.WriteLine("The length of Line using cartesian points is :" +Linelength2);
+
+            //Comparing two lengths of line using equal operator
+            if(Linelength1 == Linelength2)
+            {
+                Console.WriteLine("Both the lines are equal");
+            }
+            else if(Linelength1 > Linelength2)
+            {
+                Console.WriteLine("Line one is greater than line second");
+            }
+            else
+            {
+                Console.WriteLine("Both the lines are not equal");
+            }
         }
     }
 }
